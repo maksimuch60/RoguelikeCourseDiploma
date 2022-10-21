d5bced0ad190793678c9a530f08c8c77ad712237
DanilaAneichik <danila.aneychik2005@gmail.com>
2022-10-22 00:05:17 +0300

    Add player movement.

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    #region Variables
+
+    [SerializeField] private float _speed = 4f;
+    private Rigidbody2D _rb;
+    private bool _needToRotate;
+
+    #endregion
+
+
+    #region Unity lifecycle
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Move();
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        if (horizontal > 0 && !_needToRotate)
+        {
+            Rotate();
+        }
+        else if (horizontal < 0 && _needToRotate)
+        {
+            Rotate();
+        }
+    }
+
+    #endregion
+
+
+    #region Private methods
+
+    private void Move()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        Vector2 direction = new Vector2(horizontal, vertical);
+        Vector3 moveDelta = direction * _speed;
+        _rb.velocity = moveDelta;
+    }
+
+    private void Rotate()
+    {
+        _needToRotate = !_needToRotate;
+        transform.Rotate(0f, 180f, 0f);
+    }
+
+    #endregion
+}
