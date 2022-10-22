274db0bc049246837f0effe238f1f6b46a9e7895
DanilaAneichik <danila.aneychik2005@gmail.com>
2022-10-22 23:45:37 +0300

    Add bullet logic and bullet prefab.

---
+++ Assets/Scripts/Objects/Bullet.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace RogueLike
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class Bullet : MonoBehaviour
+    {
+        #region Variables
+
+        [SerializeField] private float _speed = 10f;
+        [SerializeField] private float _lifeTime = 3f;
+        //[SerializeField] private int _damage = 3;
+
+        private Rigidbody2D _rb;
+
+        #endregion
+
+
+        #region Unity lifecycle
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+            _rb.velocity = (transform.up * _speed);
+            StartCoroutine(LifeTimeTimer());
+        }
+
+        IEnumerator LifeTimeTimer()
+        {
+            yield return new WaitForSeconds(_lifeTime);
+
+            Destroy(gameObject);
+        }
+
+        #endregion
+    }
+}
