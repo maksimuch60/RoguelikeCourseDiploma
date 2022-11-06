b3e29c00bdf15851d6e35e80ca028bb97c896ab3
DanilaAneichik <danila.aneychik2005@gmail.com>
2022-11-06 18:02:36 +0300

    Fix rotate to enemy. Now weapon can rotate on 360 degrees.

--- Assets/Scripts/Weapon/AutoAim.cs
+++ Assets/Scripts/Weapon/AutoAim.cs
@@ -52,7 +52,7 @@ namespace RogueLike
         private void FollowEnemies()
         {
             Vector3 difference = _aimPosition.position - transform.position; 
-            float rotation = Mathf.Atan(difference.y / difference.x) * Mathf.Rad2Deg;
+            float rotation = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.Euler(0f, 0f, rotation);
         }
 
