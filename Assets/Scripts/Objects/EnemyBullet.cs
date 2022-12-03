9b6e5c27f9ec68bceb6954cdcac2569fdc68dad3
DanilaAneichik <danila.aneychik2005@gmail.com>
2022-12-03 22:37:27 +0300

    Fix rotation of bullet.

--- Assets/Scripts/Objects/EnemyBullet.cs
+++ Assets/Scripts/Objects/EnemyBullet.cs
@@ -11,32 +11,34 @@ namespace RogueLike
         private float _lifeTime = 2f;
         private Transform _player;
         private Vector2 _target;
-        private Vector3 _playerPosition; 
+        private Vector3 _playerPosition;
 
         private void Start()
         {
             _player = FindObjectOfType<PlayerHp>().transform;
 
-            _playerPosition = _player.position; 
+            _playerPosition = _player.position;
 
             _target = new Vector2(_player.position.x, _player.position.y);
             
+            Rotate();
+
             StartCoroutine(LifeTimeTimer());
         }
 
         private void Update()
         {
             transform.position = Vector2.MoveTowards(transform.position, _target, _speed * Time.deltaTime);
-            Rotate();
         }
 
         private void OnTriggerEnter2D(Collider2D col)
         {
             if (col.gameObject.CompareTag("Player"))
             {
-                col.gameObject.GetComponent<PlayerHp>().ApplyDamage(_damage);
+                col.gameObject.GetComponentInParent<PlayerHp>().ApplyDamage(_damage);
             }
         }
+
         IEnumerator LifeTimeTimer()
         {
             yield return new WaitForSeconds(_lifeTime);
@@ -46,7 +48,7 @@ namespace RogueLike
 
         private void Rotate()
         {
-            Vector3 difference = _playerPosition - transform.position; 
+            Vector3 difference = _playerPosition - transform.position;
             float rotation = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.Euler(0f, 0f, rotation);
         }
