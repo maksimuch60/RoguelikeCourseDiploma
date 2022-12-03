ad33bc31b3c2e020700c90daf84f6f1b01f259ab
DanilaAneichik <danila.aneychik2005@gmail.com>
2022-12-03 23:30:27 +0300

    Refactor enemy bullet and fix enemy run away script.

--- Assets/Scripts/Enemy/Attack/EnemyRangeAttack.cs
+++ Assets/Scripts/Enemy/Attack/EnemyRangeAttack.cs
@@ -12,7 +12,7 @@ namespace RogueLike
 
         private void Awake()
         {
-            _player = FindObjectOfType<PlayerHp>().transform; 
+            _player = FindObjectOfType<PlayerHp>().transform;
         }
 
         private void Update()
@@ -29,7 +29,7 @@ namespace RogueLike
         private void Attack()
         {
             _timer = _fireDelay;
-            Instantiate(_enemyBulletPrefab, transform.position, Quaternion.identity);
+            Instantiate(_enemyBulletPrefab, transform.position, RotateToPlayer(_player.position));
             Debug.Log("Shoot");
         }
 
@@ -41,6 +41,13 @@ namespace RogueLike
         private void TickTimer()
         {
             _timer -= Time.deltaTime;
+        }
+
+        private Quaternion RotateToPlayer(Vector3 playerPosition)
+        {
+            Vector3 difference = playerPosition - transform.position;
+            float rotation = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+            return Quaternion.Euler(0f, 0f, rotation);
         }
     }
 }
--- Assets/Scripts/Objects/EnemyBullet.cs
+++ Assets/Scripts/Objects/EnemyBullet.cs
@@ -1,56 +1,17 @@
-﻿using System.Collections;
-using Unity.VisualScripting;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace RogueLike
 {
-    public class EnemyBullet : MonoBehaviour
+    public class EnemyBullet : Bullet
     {
-        [SerializeField] private float _speed;
-        [SerializeField] private int _damage;
-        private float _lifeTime = 2f;
-        private Transform _player;
-        private Vector2 _target;
-        private Vector3 _playerPosition;
-
-        private void Start()
-        {
-            _player = FindObjectOfType<PlayerHp>().transform;
-
-            _playerPosition = _player.position;
-
-            _target = new Vector2(_player.position.x, _player.position.y);
-            
-            Rotate();
-
-            StartCoroutine(LifeTimeTimer());
-        }
-
-        private void Update()
-        {
-            transform.position = Vector2.MoveTowards(transform.position, _target, _speed * Time.deltaTime);
-        }
-
         private void OnTriggerEnter2D(Collider2D col)
         {
             if (col.gameObject.CompareTag("Player"))
             {
-                col.gameObject.GetComponentInParent<PlayerHp>().ApplyDamage(_damage);
+                col.gameObject.GetComponentInParent<PlayerHp>().ApplyDamage(Damage);
             }
         }
 
-        IEnumerator LifeTimeTimer()
-        {
-            yield return new WaitForSeconds(_lifeTime);
 
-            Destroy(gameObject);
-        }
-
-        private void Rotate()
-        {
-            Vector3 difference = _playerPosition - transform.position;
-            float rotation = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0f, 0f, rotation);
-        }
     }
 }
