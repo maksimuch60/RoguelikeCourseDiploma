cf91c39cdf6e9f40d169b406e478620d10c27a94
DanilaAneichik <danila.aneychik2005@gmail.com>
2022-12-07 23:34:37 +0300

    Add movement for melee enemies and attack for melee enemies.

--- Assets/Scripts/Enemy/Attack/EnemyAttack.cs
+++ Assets/Scripts/Enemy/Attack/EnemyAttack.cs
@@ -2,8 +2,19 @@
 
 namespace RogueLike
 {
-    public class EnemyAttack : MonoBehaviour
+    public abstract class EnemyAttack : MonoBehaviour
     {
-        
+        private float _startFireDelay;
+        protected float _timer;
+
+        protected bool CanAttack()
+        {
+            return _timer <= 0;
+        }
+
+        protected void TickTimer()
+        {
+            _timer -= Time.deltaTime;
+        }
     }
 }
---
+++ Assets/Scripts/Enemy/Attack/EnemyMeleeAttack.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace RogueLike
+{
+    public class EnemyMeleeAttack : EnemyAttack
+    {
+        [SerializeField] private int _damage; 
+        [SerializeField] private Transform _attackPoint;
+        [SerializeField] private float _radius;
+        [SerializeField] private LayerMask _layerMask;
+
+        private void Update()
+        {
+            Attack();
+        }
+
+        private void PerformDamage()
+        {
+            Collider2D col = Physics2D.OverlapCircle(_attackPoint.position, _radius, _layerMask);
+            if (col == null)
+                return;
+
+            PlayerHp playerHp = col.GetComponentInParent<PlayerHp>();
+            if (playerHp != null)
+                playerHp.ApplyDamage(_damage);
+        }
+        
+        private void Attack()
+        {
+            if (CanAttack())
+                PerformDamage();
+            Debug.Log("Now attack");
+        }
+
+        private bool CanAttack()
+        {
+            return _timer <= 0;
+        }
+
+    }
+}
