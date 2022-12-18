b7ab1022b29d3cceaeff0fb7071f9b2545c0c5ad
DanilaAneichik <danila.aneychik2005@gmail.com>
2022-12-18 23:33:53 +0300

    Add blind zone image, add canvas, add logic of blind zone mover.

---
+++ Assets/Scripts/Objects/BlindZoneMover.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace RogueLike
+{
+    public class BlindZoneMover : MonoBehaviour
+    {
+        [SerializeField] private Transform _followPlayer;
+
+        private Camera _camera;
+
+        private Transform _cachedTransform; 
+        private void Start()
+        {
+            _cachedTransform = transform; 
+            _camera = FindObjectOfType<Camera>();
+        }
+
+        private void Update()
+        {
+            Vector3 followPosition = _camera.WorldToScreenPoint(_followPlayer.position);
+            followPosition.z = _cachedTransform.position.z;
+            _cachedTransform.position = followPosition; 
+
+        }
+    }
+}
