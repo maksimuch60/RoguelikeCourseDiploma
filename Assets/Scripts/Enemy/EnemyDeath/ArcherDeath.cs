fd354b29dabbfd8c9464fe94f7f97af4cf9fb8b7
DanilaAneichik <danila.aneychik2005@gmail.com>
2022-12-20 18:11:44 +0300

    Fix archer death.

---
+++ Assets/Scripts/Enemy/EnemyDeath/ArcherDeath.cs
@@ -0,0 +1,14 @@
+﻿namespace RogueLike
+{
+    public class ArcherDeath : EnemyDeath
+    {
+        protected override void OnHpChanged(int hp)
+        {
+            if (IsDead || hp > 0)
+                return;
+            _enemyAttack.enabled = false; 
+            _enemyHp.enabled = false;
+            _enemyMovement.enabled = false;
+        }
+    }
+}
