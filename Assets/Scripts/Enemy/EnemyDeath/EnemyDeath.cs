3f1bcda7bcfb1aef2718f81a194f5ec1dcd3464f
DanilaAneichik <danila.aneychik2005@gmail.com>
2022-12-20 17:42:31 +0300

    Fix shaman death.

---
+++ Assets/Scripts/Enemy/EnemyDeath/EnemyDeath.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace RogueLike
+{
+    public abstract class EnemyDeath : MonoBehaviour
+    {
+        #region Variables
+
+        [SerializeField] protected EnemyHp _enemyHp;
+        [SerializeField] protected EnemyMovement _enemyMovement;
+        [SerializeField] protected EnemyAttack _enemyAttack;
+        
+        #endregion
+
+
+        #region Properties
+
+        protected bool IsDead { get; set; }
+
+        #endregion
+
+
+        #region Unity lifecycle
+
+        private void Start()
+        {
+            _enemyHp.OnChanged += OnHpChanged; 
+        }
+
+        #endregion
+
+
+        #region Private methods
+
+        protected abstract void OnHpChanged(int hp); 
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Enemy/EnemyDeath/ShamanDeath.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace RogueLike
+{
+    public class ShamanDeath : EnemyDeath
+    {
+        [SerializeField] private GameObject _unsafeZone;
+        [SerializeField] private EnemyAttack _complexAttack;
+        [SerializeField] private Collider2D _shamanModel; 
+        
+        protected override void OnHpChanged(int hp)
+        {
+            if (IsDead || hp > 0)
+                return;
+            _shamanModel.enabled = false; 
+            _complexAttack.enabled = false; 
+            _enemyAttack.enabled = false; 
+            _enemyHp.enabled = false;
+            _enemyMovement.enabled = false;
+            _unsafeZone.SetActive(false);
+        }
+    }
+}
