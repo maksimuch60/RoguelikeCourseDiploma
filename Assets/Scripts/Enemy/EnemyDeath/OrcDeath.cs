2715cca841fa9d9863d1fa1cc9890094dd8c6da3
DanilaAneichik <danila.aneychik2005@gmail.com>
2022-12-20 17:51:42 +0300

    Add orc death.

---
+++ Assets/Scripts/Enemy/EnemyDeath/OrcDeath.cs
@@ -0,0 +1,15 @@
+﻿namespace RogueLike
+{
+    public class OrcDeath : EnemyDeath
+    {
+        protected override void OnHpChanged(int hp)
+        {
+            if (IsDead || hp > 0)
+                return;
+
+            _enemyAttack.enabled = false;
+            _enemyHp.enabled = false;
+            _enemyMovement.enabled = false;
+        }
+    }
+}
